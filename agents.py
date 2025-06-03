689410625000da9e69dfd094b33bd12ba7d1d06c
Alexandros Popov <alexandros.popov@deepomatic.com>
2025-06-03 15:31:24 +0200

    included precommit.

--- agents.py
+++ agents.py
@@ -1,14 +1,28 @@
-from smolagents import CodeAgent, HfApiModel, tool
-from filters import *
 import os
+
+from smolagents import CodeAgent, HfApiModel
+
+import filters as flt
 
 HUGGING_FACE_TOKEN = os.environ["HUGGING_FACE_TOKEN"]
 
 # Initialize the model and agent
 model = HfApiModel(token=HUGGING_FACE_TOKEN)
-agent = CodeAgent(tools=[adjust_contrast, load_image_as_bgr, save_image], model=model)
+picture_operator = CodeAgent(
+    tools=[flt.adjust_contrast, flt.load_image_as_bgr, flt.save_image],
+    model=model,
+    name="Picture Operator",
+    description=(
+        "Performs operations on images, such as adjusting contrast, loading images, and saving them. "
+        "Give it your query as an argument, as well as the path to the image and the output path."
+    ),
+)
 
 # Run the agent
-agent.run("Adjust the contrast of the image in image_path by a factor of 1.5. Save the image to output_path.", 
-          additional_args={'image_path': 'toa-heftiba-Xmn-QXsVL4k-unsplash.jpg',
-                           "output_path": 'adjusted_image.jpg'})
+picture_operator.run(
+    "Adjust the contrast of the image in image_path by a factor of 1.5. Save the image to output_path.",
+    additional_args={
+        "image_path": "toa-heftiba-Xmn-QXsVL4k-unsplash.jpg",
+        "output_path": "adjusted_image.jpg",
+    },
+)
