2706dc5848831f1311b611a1eb160b3decaf10d8
Alexandros Popov <alexandros.popov@deepomatic.com>
2025-06-06 12:01:43 +0200

    added ops1

--- agents.py
+++ agents.py
@@ -16,6 +16,17 @@ image_operator_model = InferenceClientModel(
     max_tokens=5000,
 )
 
+picture_operator_prompt = (
+    "You are a picture operator, and you can perform operations on images, such as adjusting contrast, "
+    "loading images, and saving them. Give it your query as an argument, as well as the path to the image"
+    " and the output path."
+    "Execute only the operations that are proposed as tools. Do not invent new methods or tools."
+    "For each operation, you will receive a percentage change, like +10% or -5%."
+    "After applying the operations, you must pass the resulting image to the critic for evaluation."
+    "The critic will provide feedback on whether the change is too much, too little, or just right."
+    "You must adjust the operations based on the critic's feedback."
+)
+
 picture_operator = CodeAgent(
     tools=[
         flt.adjust_contrast,
@@ -32,15 +43,12 @@ picture_operator = CodeAgent(
         flt.add_grain,
         flt.save_image,
         flt.load_image,
+        jdg.critic,
     ],
     model=image_operator_model,
     name="PictureOperator",
-    description=(
-        "Performs operations on images, such as adjusting contrast, loading images, and saving them. "
-        "Give it your query as an argument, as well as the path to the image and the output path."
-        "Execute only the operations that are proposed as tools. Do not invent new methods or tools."
-        "If you need, simply ignore a specific operation."
-    ),
+    description=picture_operator_prompt,
+    managed_agents=[],
 )
 
 art_director_model = InferenceClientModel(
@@ -48,23 +56,25 @@ art_director_model = InferenceClientModel(
     provider="nebius",
     token=HUGGING_FACE_TOKEN,
 )
+
+art_director_prompt = (
+    "You manage the relations between the art director, the picture operator and the critic."
+    "You must present the images to improve to the art director, who will propose operations to apply to the image."
+    "The quantitative parameters in operations are hard to predict, so the art director will propose operations "
+    "with a percentage change, like +10% or -5%."
+    "You must pass those operations to the picture operator, who will apply them to the image "
+    "and refine the percentage changes with the help of the critic."
+    "Once all operations are applied, and the critic is happy, you can save the final image."
+    "You must not evaluate the image yourself, only pass it to the critic."
+    "You must not propose operations yourself, only pass them to the picture operator."
+    "You must not invent new methods or tools, only use the ones provided."
+)
 art_director = CodeAgent(
     tools=[jdg.propose_operations, jdg.critic],
     model=art_director_model,
     managed_agents=[picture_operator],
+    description=art_director_prompt,
     name="Manager",
-    description=(
-        "You manage the relations between the art director, the picture operator and the critic."
-        "You must present the images to improve to the art director, who will propose operations to apply to the image."
-        "You must then pass the operations to the picture operator, who will apply them to the image."
-        "Finally, you must present the resulting image to the critic, who will evaluate it and give feedback."
-        "You must then decide whether to continue the process or stop it based on the critic's feedback."
-        "You must not perform any operations on the image yourself, only pass them to the picture operator."
-        "You must not evaluate the image yourself, only pass it to the critic."
-        "You must not propose operations yourself, only pass them to the picture operator."
-        "You must not invent new methods or tools, only use the ones provided."
-        "If you need, simply ignore a specific operation."
-    ),
 )
 
 
@@ -74,7 +84,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--agent",
         "-a",
-        choices=["ops", "dir"],
+        choices=["ops", "ops1", "dir"],
         required=True,
         help="Which agent to run.",
     )
@@ -104,9 +114,18 @@ if __name__ == "__main__":
                 "output_path": default_output_path,
             },
         )
+    elif args.agent == "ops1":
+        directions = jdg.propose_operations(args.image_path, args.query)
+        picture_operator.run(
+            picture_operator_prompt + "\n\nuser_query : " + directions,
+            additional_args={
+                "image_path": args.image_path,
+                "output_path": default_output_path,
+            },
+        )
     elif args.agent == "dir":
         art_director.run(
-            args.query,
+            art_director_prompt + "\n\nuser_query : " + args.query,
             additional_args={
                 "image_path": args.image_path,
                 "output_path": default_output_path,
